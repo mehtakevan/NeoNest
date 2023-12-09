f2df6710c066f74ab90b29d2db97715a9c988867
Kevan <msjsci2021ema210@gmail.com>
2023-12-09 12:18:50 +0530

    sendmail

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,54 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
+const nodemailer = require("nodemailer");
+const { random } = require("colors");
+const randomstring = require("randomstring");
+
+const otpmap = new Map();
+
+const sendmail = (async(email) =>{
+
+  const otp = randomstring.generate({
+    length: 6,
+    charset: 'numeric',
+  });
+
+  otpmap.set(email,otp);
+
+  var transporter = nodemailer.createTransport({
+
+    service: 'gmail',
+    auth: {
+    
+    user: 'sdpneobank12@gmail.com',
+    pass: 'zmxa waxl giha rjeb'
+    }
+
+    
+    });
+    var mailOptions = {
+
+      from: 'sdpneobank12@gmail.com',
+      to: email,
+      subject: 'Sending Email using Node.js',
+      text: `kindly enter the otp : ${otp}`
+      };
+
+      transporter.sendMail(mailOptions, function(error, info){
+
+        if (error) {
+        
+          
+        console.log(error);
+        res.status(500);
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.status(200)
+        }
+        
+        });
+})
 
 const registerUser = asyncHandler(async (req, res) => {
     const { username, age,email, password, contact,panNumber } = req.body;
@@ -27,13 +75,18 @@ const registerUser = asyncHandler(async (req, res) => {
     });
   
     if (user) {
+      const sendmail_res = await sendmail(user.email);
+      if(sendmail_res.status === 200){
       res.status(201).json({
        _id: user._id,
         name: user.name,
         email: user.email,
-    
         token: generateToken(user._id),
       });
+    }
+    else{
+      res.status(500);
+    }
     } else {
       res.status(400);
       throw new Error("User not found");
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,36 +1,50 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userModel = mongoose.Schema(
-    {
-        id: {type: Number},
-        userName: {type: String},
-        password: { type: "String", required: true },
-        age: {type: Number},
-        email: { type: "String", unique: true, required: true },
-        contact: {
-            type: String,  // Assuming your contact number is stored as a string
-            unique: true,
-            required: true,
-        validate: {
-            validator: function(value) {
-              // Check if the value has exactly 10 digits
-              return /^\d{10}$/.test(value);
-            },
-        }
+const userModel = mongoose.Schema({
+  id: { type: Number },
+  userName: { type: String },
+  password: { type: "String", required: true },
+  age: { type: Number },
+  email: { type: "String", unique: true, required: true },
+  contact: {
+    type: String, // Assuming your contact number is stored as a string
+    unique: true,
+    required: true,
+    validate: {
+      validator: function (value) {
+        // Check if the value has exactly 10 digits
+        return /^\d{10}$/.test(value);
+      },
     },
-        panNumber: {
-            type: String,
-            unique: true,
-            required: true,
-            validate: {
-              validator: function(value) {
-                // PAN card number validation
-                // Format: ABCDE1234F (5 uppercase letters, 4 digits, 1 uppercase letter)
-                return /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/.test(value);
-              },
-            
-    
-        }
-    }
-}
-    )
+  },
+  panNumber: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: function (value) {
+        // PAN card number validation
+        // Format: ABCDE1234F (5 uppercase letters, 4 digits, 1 uppercase letter)
+        return /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/.test(value);
+      },
+    },
+  },
+});
+
+userModel.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userModel.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const Users = mongoose.model("User", userModel);
+
+module.exports = Users;
