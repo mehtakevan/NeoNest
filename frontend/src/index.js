a152e880891c49af52ae85d1f349894d695f698e
Kevan <msjsci2021ema210@gmail.com>
2024-05-02 11:47:15 +0530

    Lincecnce key

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { registerLicense } from '@syncfusion/ej2-base';
 
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCeEx3WmFZfVpgdl9GZVZTQmYuP1ZhSXxXdkBjW39ccHFXQWRYUkI=');
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCeEx3WmFZfVpgdl9GZVZTQmYuP1ZhSXxXdkBjW39ccXVVRGBfU0A=');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
